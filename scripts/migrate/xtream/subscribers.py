3e3cfa6d4cae61c9c904f6f403d1b6eeb67009ca
topilski <atopilski@gmail.com>
2019-12-27 12:35:36 -0500

    Sync

--- scripts/migrate/xtream/subscribers.py
+++ scripts/migrate/xtream/subscribers.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from pyfastocloud_models.subscriber.login.entry import SubscriberUser
+from pyfastocloud_models.subscriber.entry import Subscriber
 from pyfastocloud_models.subscriber.entry import Device
 from app.service.service import ServiceSettings
 
@@ -12,10 +12,10 @@ def import_subscribers_to_server(db, server: ServiceSettings):
     sql_subscribers = cursor.fetchall()
 
     for sql_entry in sql_subscribers:
-        new_user = SubscriberUser.make_subscriber(email=sql_entry['username'], first_name=sql_entry['username'],
+        new_user = Subscriber.make_subscriber(email=sql_entry['username'], first_name=sql_entry['username'],
                                                   last_name=sql_entry['username'], password=sql_entry['password'],
                                                   country='US', language='US')
-        new_user.status = SubscriberUser.Status.ACTIVE
+        new_user.status = Subscriber.Status.ACTIVE
         created_at = sql_entry['created_at']
         if created_at:
             new_user.created_date = datetime.fromtimestamp(created_at)
